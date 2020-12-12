8dd2301b3823c1bbd6ee987b0b9e826dba8c4398
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-12 18:11:22 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,8 @@ def speak_to_grandma(phrase)
              "I LOVE YOU TOO PUMPKIN!"
     elsif "#{phrase}" == "Hi Nana, how are you?"
          "HUH?! SPEAK UP, SONNY!"
+    elsif "#{phrase}" == "Hi!"
+         "HUH?! SPEAK UP, SONNY!"     
     elsif "#{phrase}" != "I LOVE YOU GRANDMA!"
             "NO, NOT SINCE 1938!"
     else "#{phrase}" != 'WHAT?'
